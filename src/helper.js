dffde4bc5249aaa39437eb1a9c50ad6a4aa6f6e4
Alexander Thurn <alexander.thurn@frontendsolutions.de>
2025-02-17 12:17:33 +0100

    no 16 speed

--- src/helper.js
+++ src/helper.js
@@ -644,7 +644,7 @@ function loadSpeed() {
 }
 
 function changeSpeed(oldSpeed, reverse = false) {
-    const speeds = [0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0];
+    const speeds = [0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0];
     let nextIndex
     if (!reverse) {
         nextIndex = (speeds.indexOf(oldSpeed) + 1) % speeds.length;
